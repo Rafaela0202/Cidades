b63fbcf5e5eec73f261bc4954644c96d1a5bbbc9
Rafaela0202 <rafaela.carvalho43@etec.sp.gov.br>
2021-08-29 23:09:08 -0300

    Adiciona o Exercício Cidades

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanháem");
+            Console.WriteLine("Peruibe");
+        }
+    }
+}
